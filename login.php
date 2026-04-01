ed670c76b4870b7dcddb02a1fabe106fccfd1e96
Doral <164502768+Doral-dev@users.noreply.github.com>
2026-04-01 19:23:58 +0000

    Update login.php

--- login.php
+++ login.php
@@ -52,7 +52,7 @@ try {
     $_SESSION['email'] = $user['email'];
     $_SESSION['logged_in'] = true;
 
-    header('Location: /panel.php');
+    header('Location: /dashboard.php');
     exit;
 } catch (Throwable $e) {
     exit('Error al iniciar sesión: ' . $e->getMessage());
