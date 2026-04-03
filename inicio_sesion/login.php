4d272c76f9e42605b9762efcb4d70f9d1629c260
Doral <164502768+Doral-dev@users.noreply.github.com>
2026-04-03 21:58:14 +0000

    Update login.php

--- inicio_sesion/login.php
+++ inicio_sesion/login.php
@@ -12,7 +12,7 @@ session_set_cookie_params([
 
 session_start();
 
-require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/../db.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit('Acceso no permitido');
