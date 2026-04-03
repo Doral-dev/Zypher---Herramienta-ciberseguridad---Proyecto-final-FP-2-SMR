169c5d4bd6406d425b7610b85dfc128335930e6b
Doral <164502768+Doral-dev@users.noreply.github.com>
2026-04-03 22:12:19 +0000

    Update dashboard-inicio.php

--- Dashboard/dashboard-inicio.php
+++ Dashboard/dashboard-inicio.php
@@ -17,7 +17,7 @@ $nombreEquipo = php_uname('n');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Inicio - Zypher</title>
-    <link rel="stylesheet" href="/css/dashboard.css">
+    <link rel="stylesheet" href="/Dashboard/dashboard.css">
 </head>
 <body>
 
