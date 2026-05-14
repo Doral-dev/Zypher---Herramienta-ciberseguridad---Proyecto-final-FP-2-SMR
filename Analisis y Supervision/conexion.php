5e66b4fefce745bc388a5695c266fd29cd832bda
Doral <164502768+Doral-dev@users.noreply.github.com>
2026-05-15 00:08:34 +0200

    Update conexion.php

--- Analisis y Supervision/conexion.php
+++ Analisis y Supervision/conexion.php
@@ -3,16 +3,17 @@ declare(strict_types=1);
 
 function getPDO(): PDO
 {
-    $host = 'dpg-d6rar2vafjfc73f3u5u0-a';
+    $host = 'dpg-d6rar2vafjfc73f3u5u0-a.oregon-postgres.render.com';
     $port = '5432';
     $dbname = 'zypher_db_g2sb';
     $user = 'zypher_db_g2sb_user';
     $password = 'MwoKyrgVtJaOKvqtd97QQ5yMxzvnyT86';
 
-    $dsn = "pgsql:host={$host};port={$port};dbname={$dbname};sslmode=require";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
 
     return new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_TIMEOUT => 10,
     ]);
 }
